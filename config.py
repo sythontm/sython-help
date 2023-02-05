06b4a7e2a38f780f402395335cf0359b3a2e8ea9
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-02-05 13:23:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ APP_HASH = os.environ.get("APP_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 session = os.environ.get("TERMUX")
 SESSION = os.environ.get("TERMUX")
+sython = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 ispay = ['yes']
 ispay2 = ['yes']
 
